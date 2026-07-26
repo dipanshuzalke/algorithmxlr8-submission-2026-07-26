a2c30230613f9e1a8627ea3d8be2861a41269612
Dipanshu Zalke <141048512+dipanshuzalke@users.noreply.github.com>
2026-07-26 22:39:05 +0530

    Solve Add Two Integers

---
+++ AlgorithmXlr8-P003-AddTwoIntegers/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long num1 = sc.nextLong();
+        long num2 = sc.nextLong();
+
+        System.out.println(num1 + num2);
+    }
+}
